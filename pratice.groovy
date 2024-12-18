f3093effa8752697673dfe386d75760485a7cc57
方紹軒 <jerryfang@fangshaoxuandeMacBook-Pro.local>
2024-12-18 10:27:22 +0800

    feat(): test jenkinsfile

--- pratice.groovy
+++ pratice.groovy
@@ -60,27 +60,32 @@ job('pratice-via-DSL') {
 job('robot-notification') {
     description('')
     keepDependencies(false)
-    configure { project ->
-        project / 'properties' / 'io.jenkins.plugins.DingTalkJobProperty' {
-            notifierConfigs {
-                'io.jenkins.plugins.DingTalkNotifierConfig' {
-                    raw(false)
-                    disabled(false)
-                    checked(true)
-                    robotId('4d1055b4-43e9-4536-b445-aa0b80d62c44')
-                    robotName('浪Live小幫手(測試)@機器人')
-                    atAll(false)
-                    atMobile('')
-                    content('executed the deployment of a ${PROJECT_NAME} project. (TEST MESSAGE)\n\nURL: ${JOB_URL}')
-                    message('')
-                    noticeOccasions {
-                        string('SUCCESS')
-                        string('FAILURE')
+    
+    // 使用 properties 方法來配置 DingTalk
+    properties {
+        configure { node ->
+            node << 'io.jenkins.plugins.DingTalkJobProperty' {
+                notifierConfigs {
+                    'io.jenkins.plugins.DingTalkNotifierConfig' {
+                        raw(false)
+                        disabled(false)
+                        checked(true)
+                        robotId('4d1055b4-43e9-4536-b445-aa0b80d62c44')
+                        robotName('浪Live小幫手(測試)@機器人')
+                        atAll(false)
+                        atMobile('')
+                        content('executed the deployment of a ${PROJECT_NAME} project. (TEST MESSAGE)\n\nURL: ${JOB_URL}')
+                        message('')
+                        noticeOccasions {
+                            string('SUCCESS')
+                            string('FAILURE')
+                        }
                     }
                 }
             }
         }
     }
+    
     steps {
         shell('echo \'Testing...\'')
         shell('echo ${BUILD_NUMBER}')
