c60b5160fc5ba09f09442e5cd08c5db2cc3a8982
方紹軒 <jerryfang@fangshaoxuandeMacBook-Pro.local>
2024-12-18 09:55:15 +0800

    feat(): test jenkinsfile

--- pratice.groovy
+++ pratice.groovy
@@ -10,14 +10,46 @@ job('pratice-via-DSL') {
     triggers {
         scm('* * * * *')
     }
-    
-    publishers {
-        dingtalk(
-            robot: "4d1055b4-43e9-4536-b445-aa0b80d62c44",
-            type: "MARKDOWN",
-            title: "${JOB_NAME}",
-            text: ["- 测试"]
-        )
+}
+
+job('robot-notification') {
+    description('')
+    keepDependencies(false)
+    properties {
+        // DingTalk plugin configuration
+        dingtalkJobProperty {
+            notifierConfigs {
+                dingtalkNotifierConfig {
+                    raw(false)
+                    disabled(false)
+                    checked(true)
+                    robotId('4d1055b4-43e9-4536-b445-aa0b80d62c44')
+                    robotName('浪Live小幫手(測試)@機器人')
+                    atAll(false)
+                    atMobile('')
+                    content('executed the deployment of a ${PROJECT_NAME} project. (TEST MESSAGE)\n\nURL: ${JOB_URL}')
+                    message('')
+                    noticeOccasions(['SUCCESS', 'FAILURE'])
+                }
+            }
+        }
     }
-    
+    scm {
+        // No SCM configured
+    }
+    canRoam(true)
+    disabled(false)
+    blockBuildWhenDownstreamBuilding(false)
+    blockBuildWhenUpstreamBuilding(false)
+    concurrentBuild(false)
+    steps {
+        shell('echo "Testing..."')
+        shell('echo ${BUILD_NUMBER}')
+    }
+    // publishers {
+    //     // No publishers configured
+    // }
+    // buildWrappers {
+    //     // No build wrappers configured
+    // }
 }
